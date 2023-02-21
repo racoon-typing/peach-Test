fc9c11f295b467045a9766e4acd76235974431b4
Daniil <eo18622@yandex.ru>
2023-02-21 14:06:11 +0300

    создание конфига

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './js/main.js',
+    output: {
+        filename: `./js/${filename('js')}`,
+        path: path.resolve(__dirname, 'app'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'app'),
+        },
+        open: true,
+        compress: true,
+        hot: true,
+        port: 3000,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: `./css/${filename('css')}`,
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            }
+        ]
+    }
+};
